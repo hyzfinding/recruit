3c9ea27dd77a88c332195f3ad5dc45026ffb8c95
huyuze <huyuze001@126.com>
2021-01-06 14:48:40 +0800

    提交代码

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'indent': 'off',
+    'semi': '0'
+    // 'vue/script-indent': [
+    //   'error',
+    //   4,
+    //   {
+    //     'baseIndent': 1
+    //   }
+    // ]
+  }
+};
---
+++ src/views/routerTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="routerTest">
+    <h2 @click="goRecruitManageList">招募管理</h2>
+    <h2 @click="goRecruitInterview">招募面试</h2>
+    <h2 @click="goRecruitAptitudeTestList">性向测试</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+}
+</script>
